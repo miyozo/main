85252020a8a5ff8d0c85c2f804be71d373a6954b
miyozo <110129sm@gmail.com>
2025-03-14 21:53:03 +0900

    javaにおける日付の扱い方法

---
+++ Java演習/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Date now = new Date();
+        System.out.println(now);
+        System.out.println(now.getTime());
+        Date past = new Date(1694984000000L);
+        System.out.println(past);
+
+        Calendar c = 
+    }
+}
