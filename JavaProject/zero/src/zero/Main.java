72a000279a917ae6783d1c5577699f2cfc7c8c92
miyozo <110129sm@gmail.com>
2025-03-21 03:06:22 +0900

    java

---
+++ JavaProject/zero/src/zero/Main.java
@@ -0,0 +1,20 @@
+package zero;
+
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+		ArrayList<String> al1 = new ArrayList<>(); 
+		al1.add("国語");
+		al1.add("数学");
+		al1.add("英語");
+		al1.add("社会");
+		al1.add("理科");
+		al1.add("物理");
+		
+		for(String i : al1) {
+			System.out.print(i+" ");
+		}
+	}
+
+}
