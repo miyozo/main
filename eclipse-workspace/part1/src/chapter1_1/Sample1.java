d614e8ecafee3574bfa45f6b40b674fd03b7822d
miyozo <110129sm@gmail.com>
2025-03-24 21:06:36 +0900

    java再学習中

---
+++ eclipse-workspace/part1/src/chapter1_1/Sample1.java
@@ -0,0 +1,9 @@
+package chapter1_1;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		System.out.print("Hello");
+		System.out.println(100);
+	}
+}
---
+++ eclipse-workspace/part1/src/chapter1_2/Smaple2.java
@@ -0,0 +1,10 @@
+package chapter1_2;
+
+public class Smaple2 {
+
+	public static void main(String[] args) {
+		System.out.println(1234);
+		System.out.println(123_456_789);
+	}
+
+}
