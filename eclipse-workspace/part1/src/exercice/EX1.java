e443d85a1dc17ddacefd93e0b80dd9869535a4f8
miyozo <110129sm@gmail.com>
2025-03-30 20:50:17 +0900

    java if~switch

---
+++ eclipse-workspace/part1/src/exercice/EX1.java
@@ -0,0 +1,9 @@
+package exercice;
+
+public class EX1 {
+
+	public static void main(String[] args) {
+		System.out.println("こんにちは！");
+	}
+
+}
---
+++ eclipse-workspace/part1/src/exercice/EX6_2.java
@@ -0,0 +1,18 @@
+package exercice;
+
+public class EX6_2 {
+
+	public static void main(String[] args) {
+		System.out.println(1000 + "\t 田中宏");
+		System.out.println(1010 + "\t 谷川太郎");
+		System.out.println(1020 + "\t 鈴木誠");
+		
+		System.out.println(
+				"""
+				1000		田中宏
+				1010		谷川太郎
+				1020 	鈴木誠
+				""");
+	}
+
+}
